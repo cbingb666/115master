5585433cdc598b414ec62f601a1779c7da1682ce
cbingb666 <cbingb666@gmail.com>
2026-02-20 20:20:48 +0800

    fix(monkey/drive): search bar blurs after pressing Enter

--- apps/monkey/src/components/SearchBar/SearchBar.tsx
+++ apps/monkey/src/components/SearchBar/SearchBar.tsx
@@ -42,6 +42,7 @@ const SearchBar = defineComponent({
     function handleEnter() {
       if (inputValue.value) {
         props.onEnter?.(inputValue.value)
+        inputRef.value?.blur()
       }
     }
 
