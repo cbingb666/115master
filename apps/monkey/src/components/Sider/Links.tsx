739acee64d91ca823447e46e143f4f2afa5c2d27
cbingb666 <cbingb666@gmail.com>
2026-04-12 02:54:07 +0800

    feat(Sider): add reusable Links component

---
+++ apps/monkey/src/components/Sider/Links.tsx
@@ -0,0 +1,80 @@
+import type { SlotsType } from 'vue'
+import { Icon } from '@iconify/vue'
+import { defineComponent } from 'vue'
+import PKG from '@/../package.json'
+import { useSponsorDialog } from '@/components/Sponsor/useSponsorDialog'
+import { ICON_GITHUB, ICON_QA, ICON_SPONSOR } from '@/icons'
+
+interface ExternalLinkItem {
+  icon?: string
+  text?: string
+  title?: string
+  href?: string
+  onClick?: () => void
+}
+
+const Links = defineComponent({
+  name: 'Links',
+  slots: Object as SlotsType<{
+    default: () => void
+  }>,
+  setup(_, { slots }) {
+    const openSponsor = useSponsorDialog()
+
+    const links: ExternalLinkItem[] = [
+      {
+        icon: ICON_GITHUB,
+        href: PKG.homepage,
+        title: 'GitHub',
+      },
+      {
+        icon: ICON_SPONSOR,
+        title: '赞助',
+        onClick: openSponsor,
+      },
+      {
+        icon: ICON_QA,
+        href: `${PKG.homepage}/discussions/categories/q-a`,
+        title: 'Q&A',
+      },
+      {
+        href: `${PKG.homepage}/releases/tag/v${PKG.version}`,
+        title: `V${PKG.version} Release Notes`,
+        text: `V${PKG.version}`,
+      },
+    ]
+
+    return () => (
+      <div class="flex flex-wrap gap-2">
+        {slots.default?.()}
+        {links.map(item => item.onClick
+          ? (
+              <button
+                key={item.title}
+                class="flex cursor-pointer items-center justify-between text-xs"
+                title={item.title}
+                onClick={item.onClick}
+              >
+                {item.icon && <Icon class="text-lg" icon={item.icon} />}
+                {item.text && <span class="text-base-content/50">{item.text}</span>}
+              </button>
+            )
+          : (
+              <a
+                key={item.href}
+                class="flex items-baseline-last justify-between text-xs"
+                href={item.href}
+                target="_blank"
+                title={item.title}
+              >
+                {item.icon && <Icon class="text-lg" icon={item.icon} />}
+                {item.text && <span class="text-base-content/50">{item.text}</span>}
+              </a>
+            ),
+        )}
+      </div>
+    )
+  },
+})
+
+export default Links
