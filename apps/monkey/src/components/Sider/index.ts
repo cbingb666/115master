8c5716f57516da34759867d9d92123f53d1dd10b
cbingb666 <cbingb666@gmail.com>
2026-04-12 02:59:36 +0800

    chore(Sider): update exports

--- apps/monkey/src/components/Sider/index.ts
+++ apps/monkey/src/components/Sider/index.ts
@@ -1,1 +1,4 @@
+export { default as DesktopSider } from './DesktopSider'
+export { default as Links } from './Links'
+export { default as MobileSider } from './MobileSider'
 export { default as Sider } from './Sider'
