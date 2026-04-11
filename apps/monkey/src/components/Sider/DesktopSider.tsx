a046ac1edf5bc2026dc09e66735a9dba0b9e3121
cbingb666 <cbingb666@gmail.com>
2026-04-12 02:56:36 +0800

    feat(Sider): add DesktopSider component

---
+++ apps/monkey/src/components/Sider/DesktopSider.tsx
@@ -0,0 +1,39 @@
+import type { SlotsType } from 'vue'
+import { defineComponent } from 'vue'
+import Links from './Links'
+
+const DesktopSider = defineComponent({
+  name: 'DesktopSider',
+  slots: Object as SlotsType<{
+    default: () => void
+    left: () => void
+    right: () => void
+  }>,
+  setup(_, { slots }) {
+    return () => (
+      <div
+        class="
+          border-base-content/5 bg-base-100/30
+          fixed
+          top-0 bottom-0
+          left-0 z-100
+          flex
+          w-(--sider-width)
+          flex-col
+          border-r
+          px-4
+          pb-4
+          shadow-2xl
+          max-sm:hidden
+        "
+      >
+        {slots.default?.()}
+        <Links>
+          {slots.left?.()}
+        </Links>
+      </div>
+    )
+  },
+})
+
+export default DesktopSider
