a57b2a934190da6122a1279359abc802484908ef
cbingb666 <cbingb666@gmail.com>
2026-07-18 06:25:33 +0000

    fix(monkey): 星标图标改用 STAR_FILL 实心样式并添加主题色

--- apps/monkey/src/components/FileItem/FileItemContent.tsx
+++ apps/monkey/src/components/FileItem/FileItemContent.tsx
@@ -62,13 +62,13 @@ const FileItemContent = defineComponent({
             {isStarred.value && (
               <Icon
                 class="
-                  hidden
+                  hidden text-primary
                   group-data-[view-type=card]:ml-1
                   group-data-[view-type=card]:inline-block
                   group-data-[view-type=card]:size-4
                   group-data-[view-type=card]:align-[-0.125em]
                 "
-                name={I.STAR}
+                name={I.STAR_FILL}
               />
             )}
           </span>
@@ -76,11 +76,11 @@ const FileItemContent = defineComponent({
           {isStarred.value && (
             <Icon
               class="
-                hidden shrink-0
+                hidden shrink-0 text-primary
                 group-data-[view-type=list]:inline-flex
                 group-data-[view-type=list]:size-5
               "
-              name={I.STAR}
+              name={I.STAR_FILL}
             />
           )}
         </span>
